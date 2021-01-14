410db7a67492beab1d459b5892152359b2bb3838
ali <ali.sayyah2@gmail.com>
2021-01-14 20:24:33 +0330

    added new api views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# Create your views here.
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """test api view"""
-
+    serializer_class = serializers.HelloSerializer
     def get(self, request, format=None):
         """returns a list of apiview features"""
         an_apiview = [
@@ -14,3 +15,28 @@ class HelloApiView(APIView):
             'is mapped manually to URLs',
         ]
         return Response({'massage':'hello!','an_apiview': an_apiview})
+
+    def post(self, request):
+        """create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                 status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """handle updating an object"""
+        return Response({'method':'PUT'})
+
+    def path(self, request, pk=None):
+        """handles partial update of an object"""
+        return Response({'method: ':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
